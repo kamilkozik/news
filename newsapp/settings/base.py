f194c034d04ed7c32650c8a4deb055d032acefbb
Kamil Kozik <kamil.kozik@outlook.com>
2016-01-30 15:59:13 +0100

    Database settings

--- newsapp/settings/base.py
+++ newsapp/settings/base.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-import os
+from os.path import dirname, abspath
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,9 @@ SECRET_KEY = '9c=o+i!2q4912m9ycpw2&!mzw6y#f-q&df)ty2z#66uj=!cp^='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost:8000'
+]
 
 
 # Application definition
@@ -69,17 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'newsapp.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
---
+++ newsapp/settings/dev/kk.py
@@ -0,0 +1,12 @@
+from ..base import *
+
+DATABASE = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'news',
+        'USER': 'kamil',
+        'HOST': 'localhost',
+        'PORT': '8000',
+        'PASSWORD': 'zaq12wsx',
+    }
+}
