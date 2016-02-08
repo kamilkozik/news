803c586e6d4d2c5b5dbba4faf741a8c100620a39
Kamil Kozik <kamil.kozik@outlook.com>
2016-02-08 16:26:37 +0100

    Post, Comment list display in Admin changed and API modification

--- news/models.py
+++ news/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
@@ -16,6 +17,22 @@ class Post(models.Model):
     slug = models.SlugField()
     author = models.ForeignKey(User)
 
+    class Meta:
+        verbose_name = u"Post"
+        verbose_name_plural = u"Posty"
+
+    def __str__(self):
+        return 'Post id: %s' % (self.id,)
+
+    def _switch_authorize(self):
+        self.is_authorized = (not self.is_authorized)
+
+    def _set_modification_date(self):
+        self.date_modified = timezone.now()
+
+    def _switch_commentable(self):
+        self.is_commentable = not self.is_commentable
+
     def get_absolute_url(self):
         return reverse('news:list')
 
@@ -27,6 +44,13 @@ class Comment(models.Model):
     is_authorized = models.BooleanField(default=False)
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(User)
+
+    class Meta:
+        verbose_name = u"Komentarz"
+        verbose_name_plural = u"Komentarze"
+
+    def __str__(self):
+        return ' '.join([u'ID komentarza:', str(self.pk)])
 
     def get_absolute_url(self):
         return reverse('news:list')
