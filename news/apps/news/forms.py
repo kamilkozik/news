20540378ddcf5fc64ee7335a87cbfb72e739f13b
Kamil Kozik <kamil.kozik@outlook.com>
2016-03-07 12:28:36 +0100

    Basic widget modification for PostForm

--- news/apps/news/forms.py
+++ news/apps/news/forms.py
@@ -1,21 +1,21 @@
-from django.db import models
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 
 from news.apps.news.models import Post, Comment
 
 
 class PostForm(ModelForm):
-    title = models.CharField(null=False, blank=False, max_length=500)
-    content = models.TextField(null=False, blank=False)
 
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ('title', 'content')
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-title'}),
+            'content': Textarea(attrs={'class': 'form-content', 'cols': 80})
+        }
 
 
 class CommentForm(ModelForm):
-    content = models.TextField(max_length=5000, null=False, blank=False)
 
     class Meta:
         model = Comment
-        fields = ['content']
+        fields = ('content',)
